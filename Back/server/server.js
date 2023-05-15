7b899aa8259c7ffd6cf7acf656c321acbaa7ff3e
bittorGonzalez <txort2@gmail.com>
2023-05-15 14:23:57 +0200

    Script base de datos

--- Back/server/server.js
+++ Back/server/server.js
@@ -27,8 +27,8 @@ sql.connect(dbConfig, (err) => {
 app.use(cors())
 
 // Configuración de las rutas de la API
-app.get('/', (req, res) => {
-    const query = 'USE Galys_prueba; SELECT * FROM dbo.MAESTRO_ARTICULOS';
+app.get('/maestro_articulos', (req, res) => {
+    const query = ' SELECT MA.CodigoArticulo , MA.DenomArticulo, MUC.Descripcion AS descUnidCarga, MZ.DenominacionZona, MZP.DenominacionZona as zonaPulmon FROM dbo.MAESTRO_ARTICULOS AS MA JOIN dbo.MAESTRO_UNIDADES_CARGA as MUC on MUC.CodigoUnidadCarga = MA.CodigoUnidadCarga JOIN dbo.MAESTRO_ZONAS as MZ ON MZ.CodigoZona = MA.CodigoZonaPicking JOIN dbo.MAESTRO_ZONAS as MZP ON MZP.CodigoZona = MA.CodigoZonaPulmon ';
     sql.query(query, (err, result) => {
         if (err) {
             console.log(err);
@@ -39,6 +39,18 @@ app.get('/', (req, res) => {
     });
 });
 
+
+app.get('/entradas_lineas', (req, res) => {
+    const query = ' SELECT HEL.IdLinea,HEL.CodigoArticulo, MA.DenomArticulo, MZ.DenominacionZona FROM HISTORICO_ENTRADAS_LINEAS AS HEL JOIN MAESTRO_ARTICULOS AS MA ON MA.CodigoArticulo = HEL.CodigoArticulo JOIN MAESTRO_ZONAS AS MZ ON MZ.CodigoZona = MA.CodigoZonaPicking';
+    sql.query(query, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send('Error al obtener los registros');
+        } else {
+            res.send(result.recordset);
+        }
+    });
+});
 
 
 // Iniciar el servidor
